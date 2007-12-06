678ae15257582ae93be7ff2a708fe0f8db97ee48
libc <libc@d935cd10-9f40-0410-bf43-b8529b0c744a>
2007-12-06 15:37:19 +0000

    initial import of 0.17.y branch
    git-svn-id: https://xchataqua.svn.sourceforge.net/svnroot/xchataqua/trunk@1 d935cd10-9f40-0410-bf43-b8529b0c744a

---
+++ X-Chat Aqua/config.h
@@ -0,0 +1,23 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "XChat"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "XChat 2.8.4"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "xchat"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.8.4"
+
+#define XCHATLIBDIR "/usr/local/lib"
+
+#define MYVERSION XCHAT_AQUA_VERSION
+
+#define HAVE_SNPRINTF  1
+#define HAVE_VSNPRINTF 1
+#include "glibconfig.h"
+#include <stdio.h>
+#endif
